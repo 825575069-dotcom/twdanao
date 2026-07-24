6054fd8761d758584a73ae8179f50dfbf04f27f1
chenshenghe <825575069@qq.com>
2026-07-24 16:54:16 +0800

    feat: 添加 wsgi.py 用于 Gunicorn 部署

---
+++ yesgo-backend/config/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for YesGo 天网大脑后端
+"""
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+# 将项目根目录加入 Python 路径
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# 默认使用 MySQL 生产环境
+os.environ.setdefault('DB_ENGINE', 'mysql')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = get_wsgi_application()
